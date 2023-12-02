01ff0cf35026490b09db04a417939a5b5f1ce3fd
Matthieu Bardal <mattbardal@gmail.com>
2023-12-02 13:39:22 -0700

    Cleaned 02.rb

--- 2023/02/02.rb
+++ 2023/02/02.rb
@@ -7,42 +7,38 @@ R, G, B = 12, 13, 14
 def parse_game_input(game)
   possible = true
   m_r, m_g, m_b = 0, 0, 0
-  game_num, rounds = game.split(":")
-  id = game_num.split(" ").last.to_i
+  game_num, rounds = game.split(": ")
+  id = game_num.split.last.to_i
 
-  rounds.split(";").each do |round|
+  rounds.split("; ").each do |round|
     r, g, b = 0, 0, 0
-    round.split(",").each do |cubes|
-      count, color = cubes.strip.split(" ")
+    round.split(", ").each do |cubes|
+      count, color = cubes.split
       count = count.to_i
 
       case color.downcase
       when "red"
         r += count
-        m_r = r if r > m_r
+        m_r = [r, m_r].max
       when "green"
         g += count
-        m_g = g if g > m_g
+        m_g = [g, m_g].max
       when "blue"
         b += count
-        m_b = b if b > m_b
+        m_b = [b, m_b].max
       end
     end
-    if r > R || g > G || b > B
-      possible = false
-    end
+    possible = false if r > R || g > G || b > B
   end
-  return (possible ? id : 0), m_r * m_g * m_b
+  [possible ? id : 0, m_r * m_g * m_b]
 end
 
 possible_games = []
 game_powers = []
 
-input.each_line do |line|
-  id, power = parse_game_input(line.delete("\n"))
-  possible_games.append(id)
-  game_powers.append(power)
-end
+possible_games, game_powers = input.each_line.map do |line|
+  parse_game_input(line.chomp)
+end.transpose
 
 puts possible_games.sum
 puts game_powers.sum
